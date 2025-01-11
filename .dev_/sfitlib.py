0492d919552762aa8aae9820dffa27e31869c792
Roch Schanen <r.schanen@lancaster.ac.uk>
2025-01-11 16:11:25 +0000

    add sfitlib header

--- .dev_/sfitlib.py
+++ .dev_/sfitlib.py
@@ -1,12 +1,47 @@
+# file: sfitlib.py
+# content: collection of fitting functions
+# Created: 11 january 2025
+# Author: Roch Schanen
+# comments: this library is in development
 
+version_history = {}
+
+""" 
+
+    --- motivation ---
+
+"""
 
 # from package: "https://numpy.org/"
-from numpy import pi, cos, sin
+# ----------------------------------
+
+from numpy import pi
+from numpy import cos
+from numpy import sin
 
 # from package: "https://scipy.org/"
+# ----------------------------------
+
 from scipy.optimize import curve_fit as fit
 
-# ROTATE BY AN ANGLE "A" BEFORE DISPLAY
+########################
+## lorentz functions ###
+########################
+
+# # Aborption
+# def FX(t, p, w, h, o):
+#     x = (t-p)/w
+#     y = h/(1+square(x))
+#     return o + y
+
+# # Dispersion
+# def FY(t, p, w, h, o):
+#     x = (t-p)/w
+#     y = x*h/(1+square(x))
+#     # inverse sign on request
+#     return o - y
+
+# ROTATE BY AN ANGLE "A"
 # def Rotate
 # a_deg   = 20.0
 # a_rad   = a_deg*pi/180
@@ -16,47 +51,33 @@ from scipy.optimize import curve_fit as fit
 # # To adjust the lockin, substract the
 # # value of a from the lockin phase.
 
+# # define starting parameters and fit
+# parSx = [32705.0, 5.0, 1.0,  2.0]
+# parSy = [32705.0, 5.0, 1.0, 18.0]
 
-# Aborption
-def FX(t, p, w, h, o):
-    x = (t-p)/w
-    y = h/(1+square(x))
-    return o + y
+# # parX = parSx
+# parX, parXC = fit(FX, F, X, p0 = parSx)
+# # parY = parSy
+# parY, parYC = fit(FY, F, Y, p0 = parSy)
 
-# Dispersion
-def FY(t, p, w, h, o):
-    x = (t-p)/w
-    y = x*h/(1+square(x))
-    # inverse sign on request
-    return o - y
+# # plot fits
+# ax.plot(F, FX(F, *parX), "--k", linewidth = 0.6)
+# ax.plot(F, FY(F, *parY), "--k", linewidth = 0.6)
 
-# define starting parameters and fit
-parSx = [32705.0, 5.0, 1.0,  2.0]
-parSy = [32705.0, 5.0, 1.0, 18.0]
+# for l in L[:10]:
+#     t += l[2:]
+# t += f"\nrotation : {-a_deg} degrees"
+# headerText(t, fg)
 
-# parX = parSx
-parX, parXC = fit(FX, F, X, p0 = parSx)
-# parY = parSy
-parY, parYC = fit(FY, F, Y, p0 = parSy)
+# # export fit results to footer text
+# t =  f"                   phase      quadrature\n\n"
+# t += f"position :{parX[0]:12.3f}Hz, {parY[0]:12.3f}Hz\n"
+# t += f"width    :{parX[1]:12.3f}Hz, {parY[1]:12.3f}Hz\n"
+# t += f"height   :{parX[2]:12.3f}{s}V, {parY[2]:12.3f}{s}V\n"
+# t += f"offset   :{parX[3]:12.3f}{s}V, {parY[3]:12.3f}{s}V\n"
+# footerText(t, fg)
 
-# plot fits
-ax.plot(F, FX(F, *parX), "--k", linewidth = 0.6)
-ax.plot(F, FY(F, *parY), "--k", linewidth = 0.6)
-
-for l in L[:10]:
-    t += l[2:]
-t += f"\nrotation : {-a_deg} degrees"
-headerText(t, fg)
-
-# export fit results to footer text
-t =  f"                   phase      quadrature\n\n"
-t += f"position :{parX[0]:12.3f}Hz, {parY[0]:12.3f}Hz\n"
-t += f"width    :{parX[1]:12.3f}Hz, {parY[1]:12.3f}Hz\n"
-t += f"height   :{parX[2]:12.3f}{s}V, {parY[2]:12.3f}{s}V\n"
-t += f"offset   :{parX[3]:12.3f}{s}V, {parY[3]:12.3f}{s}V\n"
-footerText(t, fg)
-
-headerText = ""
-for k in info.keys():
-    headerText += f"{k:<8}: {info[k]}\n"
+# headerText = ""
+# for k in info.keys():
+#     headerText += f"{k:<8}: {info[k]}\n"
 
