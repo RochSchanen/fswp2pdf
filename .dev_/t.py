e217445a5f8c727cce1ba2edc1ce09119a52af6b
Roch Schanen <r.schanen@lancaster.ac.uk>
2025-01-15 21:46:43 +0000

    add test: not working

---
+++ .dev_/t.py
@@ -0,0 +1,12 @@
+# ROTATE BY AN ANGLE "A"
+# def Rotate
+# a_deg   = 20.0
+# a_rad   = a_deg*pi/180
+# xr      = X*cos(a_rad)-Y*sin(a_rad)
+# yr      = X*sin(a_rad)+Y*cos(a_rad)
+# X, Y    = xr, yr
+# # To adjust the lockin, subtract the
+# # value of a from the lockin phase.
+
+# t += f"\nrotation : {-a_deg} degrees"
+# headerText(t, fg)
---
+++ .dev_/test.py
@@ -0,0 +1,4 @@
+import fswp2pdf
+
+fswp2pdf.export("../.data/fswp_full_1.dat")
+
